77ef1ab11bf20e9ecb699b914a4dc1a51d21ffc3
APlyusnin <a.plyusnin@g.nsu.ru>
2022-11-18 01:18:06 +0700

    Fix

---
+++ src/main/java/ru/nsu/fit/hmtl/inference/context/ContextException.java
@@ -0,0 +1,8 @@
+package ru.nsu.fit.hmtl.inference.context;
+
+/** An exception on type inference in the context. */
+public class ContextException extends RuntimeException {
+	public ContextException(String message) {
+		super(message);
+	}
+}
