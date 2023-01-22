e861dffb434285660f94ded3a593bb3ab5e365cd
Simon Morozov <42973527+shiftuya@users.noreply.github.com>
2023-01-23 01:04:04 +0700

    Update lambda functions (#23)
    * Update lambda initialization
    
    * Override variable names in function definitions & let

--- src/main/java/ru/nsu/fit/hmtl/parsing/HMTLListener.java
+++ src/main/java/ru/nsu/fit/hmtl/parsing/HMTLListener.java
@@ -6,14 +6,21 @@ import ru.nsu.fit.hmtl.core.typesystem.table.TypeTable;
 import ru.nsu.fit.hmtl.core.typesystem.types.Type;
 import ru.nsu.fit.hmtl.source.tree.*;
 
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class HMTLListener extends ClojureBaseListener {
 
+    private int lambdaCount = 0;
+
+    private int variableAliasCount = 0;
+
+    private final Map<String, Stack<String>> variableAlias = new HashMap<>();
+
     private final Stack<TreeNode> stack = new Stack<>();
 
-    private static class FakeLetNode extends TreeNode {
+    private class FakeLetNode extends TreeNode {
+
+        private final Stack<String> aliases = new Stack<>();
 
         @Override
         public Expression generateExpression() {
@@ -35,18 +42,21 @@ public class HMTLListener extends ClojureBaseListener {
 
         }
 
-        LetNode getRoot() {
-            LetNode res = new LetNode();
-            LetNode cur = res;
-            for (int i = 0; i < children.size(); ++i) {
-                if (i % 2 == 0 && i != 0 && i != children.size() - 1) {
-                    LetNode newNode = new LetNode();
-                    cur.addChild(newNode);
-                    cur = newNode;
-                }
-                cur.addChild(children.get(i));
+        TreeNode getRoot() {
+            TreeNode cur = children.get(children.size() - 1);
+            for (int i = children.size() - 3; i >= 0; i -= 2) {
+                TreeNode node = new LetNode();
+                node.addChild(children.get(i));
+                node.addChild(children.get(i + 1));
+                node.addChild(cur);
+                freeGeneratedAlias(aliases.pop());
+                cur = node;
             }
-            return res;
+            return cur;
+        }
+
+        void addAlias(String alias) {
+            aliases.push(alias);
         }
     }
 
@@ -96,6 +106,13 @@ public class HMTLListener extends ClojureBaseListener {
             stack.peek().addChild(node);
             stack.push(node);
         } else if (ctx.children.size() != 0
+                && ctx.getChild(1).getChild(0).getText().equals("fn")) {
+            String functionName = generateLambdaName();
+            var node = new AbstractionNode();
+            node.addChild(new VariableNode(functionName, TypeTable.getInstance().createVaryingType()));
+            stack.peek().addChild(node);
+            stack.push(node);
+        } else if (ctx.children.size() != 0
                 && ctx.getChild(1).getChild(0).getText().equals("let")) {
             var node = new FakeLetNode();
 
@@ -109,10 +126,22 @@ public class HMTLListener extends ClojureBaseListener {
 
     @Override
     public void exitList_(ClojureParser.List_Context ctx) {
+        if (stack.peek() instanceof AbstractionNode) {
+            for (int i = 0; i < ctx.getChild(1).getChildCount() - 1; ++i) {
+                if (ctx.getChild(1).getChild(i).getText().equals("defn")) {
+                    i++; // skip function name
+                    continue;
+                }
+                if (ctx.getChild(1).getChild(i).getText().equals("fn")) {
+                    continue;
+                }
+                freeAlias(ctx.getChild(1).getChild(i).getText());
+            }
+        }
         if (ctx.children.size() != 0
                 && ctx.getChild(1).getChild(0).getText().equals("let")) {
             FakeLetNode fakeLetNode = (FakeLetNode) stack.pop();
-            LetNode node = fakeLetNode.getRoot();
+            TreeNode node = fakeLetNode.getRoot();
             stack.peek().addChild(node);
             stack.push(node);
         }
@@ -207,23 +236,26 @@ public class HMTLListener extends ClojureBaseListener {
     @Override
     public void enterSymbol(ClojureParser.SymbolContext ctx) {
 
-        if (stack.peek() instanceof VariableNode || ctx.getText().equals("let") || ctx.getText().equals("defn")) return;
+        if (stack.peek() instanceof VariableNode || ctx.getText().equals("let") || ctx.getText().equals("defn") || ctx.getText().equals("fn")) return;
         if (stack.peek() instanceof AbstractionNode) {
-            if (ctx.getParent().getParent().getParent().getChild(1).equals(ctx.getParent().getParent())) {
+            if (ctx.getParent().getParent().getParent().getChild(1).equals(ctx.getParent().getParent())
+            && ctx.getParent().getParent().getParent().getChild(0).getText().equals("defn")) {
                 stack.peek().addChild(new VariableNode(ctx.getText(), TypeTable.getInstance().createVaryingType()));
             } else {
-                stack.peek().addChild(new VariableNode(ctx.getText(), TypeTable.getInstance().createVaryingType()));
+                stack.peek().addChild(new VariableNode(generateAlias(ctx.getText()), TypeTable.getInstance().createVaryingType()));
             }
         } else if (stack.peek() instanceof FakeLetNode) {
+            String alias = generateAlias(ctx.getText()); // todo
             for (int i = 1; i < ctx.getParent().getParent().getParent().getChildCount(); ++i) {
                 if (ctx.getParent().getParent().getParent().getChild(i).equals(ctx.getParent().getParent())) {
-                    stack.peek().addChild(new VariableNode(ctx.getText(), TypeTable.getInstance().createVaryingType()));
+                    stack.peek().addChild(new VariableNode(alias, TypeTable.getInstance().createVaryingType()));
+                    ((FakeLetNode) stack.peek()).addAlias(alias);
                     return;
                 }
             }
-            stack.peek().addChild(new VariableNode(ctx.getText(), TypeTable.getInstance().createVaryingType()));
+            stack.peek().addChild(new VariableNode(alias, TypeTable.getInstance().createVaryingType()));
         } else {
-            stack.peek().addChild(new ConstantNode(ctx.getText()));
+            stack.peek().addChild(new ConstantNode(getAlias(ctx.getText())));
 
         }
     }
@@ -235,5 +267,34 @@ public class HMTLListener extends ClojureBaseListener {
     }
 
 
+    private String generateLambdaName() {
+        String name = "__lambda#" + lambdaCount;
+        lambdaCount++;
+        return name;
+    }
 
+    private String generateAlias(String name) {
+        String alias = "__" + name + "#" + variableAliasCount;
+        variableAliasCount++;
+        if (!variableAlias.containsKey(name)) {
+            variableAlias.put(name, new Stack<>());
+        }
+        variableAlias.get(name).push(alias);
+        return alias;
+    }
+
+    private void freeAlias(String name) {
+        variableAlias.get(name).pop();
+    }
+
+    private void freeGeneratedAlias(String name) {
+        freeAlias(name.split("#")[0].substring(2));
+    }
+
+    private String getAlias(String name) {
+        if (variableAlias.containsKey(name)) {
+            return variableAlias.get(name).peek();
+        }
+        return name;
+    }
 }
